1f2afdf82c008d7b2aa53adfca715fd912ec907e
David Lewis <lewisde@gmail.com>
2020-04-25 15:35:27 -0400

    Implemented font dialog change. Still need to save setting on exit.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,4 +1,10 @@
+#include "addressdialog.h"
+#include "latedialog.h"
+#include "logindialog.h"
 #include "mainwindow.h"
+#include "QDebug"
+#include "QFontDialog"
+
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
@@ -17,42 +23,51 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_btnLogin_clicked()
 {
-    printf("btnLogin clicked\n");
+    loginDialog *ld = new loginDialog();
+    ld->exec();
+    qDebug("btnLogin clicked\n");
 }
 
 void MainWindow::on_comboClasses_currentIndexChanged(const QString &arg1)
 {
-    printf("comboClass changed %s\n", arg1.toStdString().c_str());
+    qDebug("comboClass changed %s\n", arg1.toStdString().c_str());
 }
 
 void MainWindow::on_btnSubmit_clicked()
 {
-    printf("btnSubmit clicked\n");
+    qDebug("btnSubmit clicked\n");
 }
 
 void MainWindow::on_btnLogout_clicked()
 {
-    printf("btnLogout clicked\n");
+    qDebug("btnLogout clicked\n");
 }
 
 
 void MainWindow::on_actionQuit_triggered()
 {
-    printf("actionQuit triggered\n");
+    qDebug("actionQuit triggered\n");
     exit(0);
 }
 
 void MainWindow::on_actionChange_Font_triggered()
 {
-    printf("fontDialog triggered\n");
+    bool ok;
+    QFont font = QFontDialog::getFont(&ok);
+    qApp->setFont(font);
+    qDebug("fontDialog triggered\n");
 }
 
 void MainWindow::on_actionBlackboard_Address_triggered()
 {
-    printf("loginDialog triggered\n");
+    addressDialog *ad = new addressDialog();
+    ad->exec();
+    qDebug("loginDialog triggered\n");
 }
 
 void MainWindow::on_actionLate_Threshold_triggered()
 {
-    printf("lateDialog triggered\n");
+    lateDialog *ld = new lateDialog();
+    ld->exec();
+    qDebug("lateDialog triggered\n");
 }
